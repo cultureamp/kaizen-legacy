807e2d46a6480aa435d2ad76c8ca175f2e77116d
Michael Winter <mcwinter07@gmail.com>
2023-08-28 16:48:34 +1000

    add fix for unmount change

--- packages/rich-text-editor/src/Toolkit/plugins/LinkManager/createReactTooltipWrapper.tsx
+++ packages/rich-text-editor/src/Toolkit/plugins/LinkManager/createReactTooltipWrapper.tsx
@@ -1,22 +1,23 @@
 import React, { ElementType, useEffect, useState } from "react"
-import ReactDOM from "react-dom"
 import Nanobus from "nanobus"
+import { createRoot } from "react-dom/client"
 import { LinkEditorProps } from "./components"
 
+type WrapperProps = {
+  emitter: Nanobus
+  componentProps: LinkEditorProps
+  Component: ElementType
+}
 function Wrapper({
   emitter,
   componentProps,
   Component,
-}: {
-  emitter: Nanobus
-  componentProps: LinkEditorProps
-  Component: ElementType
-}) {
+}: WrapperProps): JSX.Element {
   const [localComponentProps, setLocalComponentProps] =
     useState<LinkEditorProps>(componentProps)
 
   useEffect(() => {
-    function onUpdate(newComponentProps: LinkEditorProps) {
+    const onUpdate = (newComponentProps: LinkEditorProps): void => {
       setLocalComponentProps(newComponentProps)
     }
     emitter.addListener("update", onUpdate)
@@ -33,31 +34,30 @@ function Wrapper({
  * Create a wrapping connector for rendering a React element into a separate
  * part of the DOM while being able to update it without remounting.
  */
-export function createReactTooltipWrapper(
+export const createReactTooltipWrapper = (
   parentNode: HTMLElement,
   Component: ElementType,
   componentProps: LinkEditorProps
-) {
+): { destroy: () => void; update: (props: LinkEditorProps) => void } => {
   const emitter = new Nanobus()
   const node = document.createElement("div")
   parentNode.appendChild(node)
+  const root = createRoot(node)
 
-  ReactDOM.render(
+  root.render(
     <Wrapper
       componentProps={componentProps}
       Component={Component}
       emitter={emitter}
-    />,
-    node
+    />
   )
 
-  function destroy() {
-    ReactDOM.unmountComponentAtNode(node)
+  function destroy(): void {
+    root.unmount()
     emitter.removeAllListeners("*")
-    node.remove()
   }
 
-  function update(props: LinkEditorProps) {
+  function update(props: LinkEditorProps): void {
     emitter.emit("update", props)
   }
 
