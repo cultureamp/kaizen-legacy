45d31443a3cebd738b8adf1076075393efd70134
kaydwithers <kaydwithers@gmail.com>
2023-08-31 22:52:36 +1000

    fix: add xs spacing for TextField input

--- draft-packages/form/KaizenDraft/Form/TextField/TextField.tsx
+++ draft-packages/form/KaizenDraft/Form/TextField/TextField.tsx
@@ -85,6 +85,7 @@ export const TextField = ({
         id={`${id}-field-input`}
         automationId={`${id}-field-input`}
         ariaDescribedBy={ariaDescribedBy}
+        classNameOverride={styles.textFieldInput}
         disabled={disabled}
         reversed={reversed}
         status={status}
